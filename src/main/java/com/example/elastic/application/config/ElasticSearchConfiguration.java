835ade08c186d8f508a6734bcfa96adcc7b709cf
rafa-aleman <rafael.aleman@gmail.com>
2016-11-15 10:46:15 +0100

    Simplify the configuration of the PropertyNamingStrategy using @JsonNaming

--- src/main/java/com/example/elastic/application/config/ElasticSearchConfiguration.java
+++ src/main/java/com/example/elastic/application/config/ElasticSearchConfiguration.java
@@ -1,15 +1,11 @@
 package com.example.elastic.application.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.elasticsearch.client.Client;
 import org.elasticsearch.common.settings.Settings;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
-import org.springframework.data.elasticsearch.core.EntityMapper;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
-
-import java.io.IOException;
 
 import static org.elasticsearch.node.NodeBuilder.nodeBuilder;
 
@@ -30,26 +26,6 @@ public class ElasticSearchConfiguration {
 
 	@Bean
 	public ElasticsearchTemplate elasticsearchTemplate() {
-		return new ElasticsearchTemplate(client(), new EntityMapperImpl());
-	}
-
-	@Bean
-	public ObjectMapper objectMapper() {
-		final ObjectMapper objectMapper = new ObjectMapper();
-		objectMapper.setPropertyNamingStrategy(new MyPropertyNamingStrategy());
-		return objectMapper;
-	}
-
-	public class EntityMapperImpl implements EntityMapper {
-
-		private final ObjectMapper objectMapper = objectMapper();
-
-		public String mapToString(Object object) throws IOException {
-			return objectMapper.writeValueAsString(object);
-		}
-
-		public <T> T mapToObject(String source, Class<T> clazz) throws IOException {
-			return objectMapper.readValue(source, clazz);
-		}
+		return new ElasticsearchTemplate(client());
 	}
 }
